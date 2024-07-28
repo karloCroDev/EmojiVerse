9e353fe5f285ca3ec6d24a98d24f2516e2134af3
karloCroDev <karlo.webdev@gmail.com>
2024-07-28 16:07:00 +0200

    UI for ChangeProfileModal is created

--- src/app/(main)/components/modals/ChangeProfileModal.tsx
+++ src/app/(main)/components/modals/ChangeProfileModal.tsx
@@ -1,30 +1,86 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import {
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogFooter,
+  DialogClose,
 } from "@/components/ui/dialog";
-
+import { FaCamera } from "react-icons/fa";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import { Button } from "@/components/ui/button";
+
 const ChangeProfileModal = () => {
+  const username = "Ana Horvat";
+  const { push } = useRouter();
+
+  const [changeUsername, setChangeUsername] = useState("");
+  const [changeBio, setChangeBio] = useState("");
+  const [changePassword, setChangePassword] = useState("");
+
   return (
     <DialogContent>
       <DialogHeader>
         <DialogTitle>Your profile</DialogTitle>
       </DialogHeader>
-      <div className="w-full flex flex-col items-center border">
-        <Avatar className="w-[10rem] h-[10rem]">
-          <AvatarImage alt="Profile picture" />
-          <AvatarFallback className="text-4xl">IH</AvatarFallback>
-        </Avatar>
+      <hr className="mt-[-4px]" />
+      <div className="w-full flex flex-col items-center gap-y-4">
+        <label htmlFor="picture-id">
+          <Avatar className="w-[10rem] h-[10rem] cursor-pointer relative !overflow-visible">
+            <AvatarImage alt="Profile picture" />
+            <AvatarFallback className="text-4xl">IH</AvatarFallback>
+            <FaCamera className="absolute size-8 z-10 right-5 bottom-0" />
+          </Avatar>
+        </label>
+        <input type="file" className="hidden" id="picture-id" />
+        <h1 className="text-3xl font-bold ">Ivan Horvat</h1>
+        <hr className="w-[90%]" />
+        <label className="div-label">
+          <h3 className="div-label-h3">Username</h3>
+          <input
+            type="text"
+            placeholder="...new username"
+            className="form-label-input"
+            onChange={(e) => setChangeUsername(e.target.value)}
+          />
+        </label>
+        <label className="div-label">
+          <h3 className="div-label-h3">Password</h3>
+          <input
+            type="text"
+            placeholder="...new username"
+            className="form-label-input"
+            onChange={(e) => setChangePassword(e.target.value)}
+          />
+        </label>
+        <label className="div-label">
+          <h3 className="div-label-h3 ">Bio</h3>
+          <input
+            type="text"
+            placeholder="...new bio"
+            className="form-label-input"
+            onChange={(e) => setChangeBio(e.target.value)}
+          />
+        </label>
+        <hr className="w-[90%]" />
       </div>
-      <DialogFooter className="flex !justify-between w-full">
-        <button className="text-secondary">Public profile</button>
-        <Button>Save changes</Button>
+      <DialogFooter>
+        <DialogClose className="flex justify-between items-center w-full ">
+          <button
+            className="text-secondary"
+            onClick={
+              () => push(`/${username}`)
+              //I had to do it like this because modal won't close on imported Link component from next.js
+            }
+          >
+            Public profile
+          </button>
+          <Button>Save changes</Button>
+        </DialogClose>
       </DialogFooter>
     </DialogContent>
   );
