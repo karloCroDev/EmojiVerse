2c12af8473c9c9113a6b7e914693d1506bd3abae
karloCroDev <karlo.webdev@gmail.com>
2024-08-15 18:17:41 +0200

    Filters are completed

--- src/app/(main)/main-page/components/PostsFilter.tsx
+++ src/app/(main)/main-page/components/PostsFilter.tsx
@@ -11,12 +11,18 @@ const PostsFilter = ({ posts }: { posts: any[] }) => {
   }));
   const randomSelection = 0.5 - Math.random();
 
-  const uid = useAuthState((state) => ({
-    followers: state.uid,
-  }));
+  const uid = useAuthState((state) => state.uid);
+
   return (
     <>
       {posts
+        .filter((post) =>
+          filterSelect === "following"
+            ? post.followers.includes(uid)
+              ? post
+              : null
+            : post
+        )
         .filter(
           (post) =>
             post.username.includes(filterInput) ||
@@ -28,22 +34,21 @@ const PostsFilter = ({ posts }: { posts: any[] }) => {
               return b.followers.length - a.followers.length;
             case "recommended":
               return randomSelection;
-
+            case "following":
+              return b.followers.length - a.followers.length;
             default:
               return randomSelection;
           }
         })
         .map((post) => {
           return (
-            <>
-              <Posts
-                key={post.authorUid}
-                bio={post.bio}
-                content={post.content}
-                pfp={post.pfp}
-                username={post.username}
-              />
-            </>
+            <Posts
+              key={post.authorUid}
+              bio={post.bio}
+              content={post.content}
+              pfp={post.pfp}
+              username={post.username}
+            />
           );
         })}
     </>
